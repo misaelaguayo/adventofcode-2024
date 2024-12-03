ac2963c9349bbb50440ed50610199688564513cf
Misael Aguayo <aguayo.misael@gmail.com>
2024-12-02 19:53:30 -0600

    Day 2 part 1

---
+++ src/day2_1.rs
@@ -0,0 +1,45 @@
+use std::{fs, i32};
+
+pub fn ans() -> i32 {
+    let contents = fs::read_to_string("input2_1").expect("Should have been able to read the file");
+    let lines: Vec<String> = contents.lines().map(String::from).collect();
+
+    let mut res = 0;
+
+    for line in &lines {
+        let mut is_increasing = false;
+        let mut is_decreasing = false;
+
+        let mut is_safe = true;
+        let level: Vec<i32> = line
+            .split_whitespace()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect();
+
+        for i in 1..level.len() {
+            let diff = (level[i] - level[i - 1]).abs();
+
+            if diff == 0 || diff > 3 {
+                is_safe = false;
+            }
+
+            if level[i] > level[i - 1] {
+                is_increasing = true;
+            }
+
+            if level[i] < level[i - 1] {
+                is_decreasing = true;
+            }
+        }
+
+        if is_increasing && is_decreasing {
+            is_safe = false;
+        }
+
+        if is_safe {
+            res += 1;
+        }
+    }
+
+    res
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod day1_1;
 mod day1_2;
+mod day2_1;
 
 fn main() {
     let _ans1_1 = day1_1::ans();
-    let ans1_2 = day1_2::ans();
-    println!("{ans1_2}");
+    let _ans1_2 = day1_2::ans();
+    let ans2_1 = day2_1::ans();
+
+    println!("{ans2_1}");
 }
