42fd53a76d85d1cad858d490362fa3d830f55ff7
Misael Aguayo <maguayo@armlend.com>
2024-12-03 15:04:47 -0600

    Day 3 part 1

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ mod day1_1;
 mod day1_2;
 mod day2_1;
 mod day2_2;
+mod day3_1;
 
 fn main() {
     let _ans1_1 = day1_1::ans();
     let _ans1_2 = day1_2::ans();
     let _ans2_1 = day2_1::ans();
-    let ans2_2 = day2_2::ans();
+    let _ans2_2 = day2_2::ans();
+    let ans3_1 = day3_1::ans();
 
-    println!("{ans2_2}");
+    println!("{ans3_1}");
 }
