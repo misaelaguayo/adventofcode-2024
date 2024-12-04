021180ecee2070fa59cd868776dcb48972a5eabd
Misael Aguayo <maguayo@armlend.com>
2024-12-03 22:18:49 -0600

    Day 3 part 2

--- src/day3_2.rs
+++ src/day3_2.rs
@@ -3,13 +3,25 @@ use std::{fs, i32};
 use regex::Regex;
 
 pub fn ans() -> i32 {
-    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
 
     let contents = fs::read_to_string("inputs/input3_1").expect("Should have been able to read the file");
     let lines: Vec<String> = contents.lines().map(String::from).collect();
     let complete = lines.join("");
 
-    let num_matches: Vec<(i32, i32)> = re.captures_iter(&complete).map(|cap| {
+    let mut i = 7;
+    let mut new_complete = "".to_owned();
+    while i < complete.len(){
+        if &complete[i-7..i].to_string() == "don't()"{
+            while &complete[i-4..i].to_string() != "do()" && i < complete.len(){
+                i += 1
+            }
+        }
+        new_complete.push_str(&complete[i..i+1]);
+        i += 1;
+    }
+
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let num_matches: Vec<(i32, i32)> = re.captures_iter(&new_complete).map(|cap| {
         let (_, [num1, num2]) = cap.extract();
         (num1.to_string().parse::<i32>().unwrap(), num2.to_string().parse::<i32>().unwrap())
     }).collect();
@@ -18,3 +30,4 @@ pub fn ans() -> i32 {
 
     products.iter().sum::<i32>()
 }
+
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ mod day1_2;
 mod day2_1;
 mod day2_2;
 mod day3_1;
+mod day3_2;
 
 fn main() {
     let _ans1_1 = day1_1::ans();
     let _ans1_2 = day1_2::ans();
     let _ans2_1 = day2_1::ans();
     let _ans2_2 = day2_2::ans();
-    let ans3_1 = day3_1::ans();
+    let _ans3_1 = day3_1::ans();
+    let ans3_2 = day3_2::ans();
 
-    println!("{ans3_1}");
+    println!("{ans3_2}");
 }
